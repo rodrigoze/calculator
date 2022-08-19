c36e215696de984ce9691356637ebcfd507ea5ed
rodrigo <coinbonusorg@gmail.com>
2022-08-19 01:47:28 +0100

    index.html

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const btns = document.querySelectorAll("button");
+const btns = document.querySelectorAll("[data-number]");
+const o = document.querySelectorAll("[operator]");
 const display = document.getElementById("display");
-let num;
+const displayz = document.getElementById("displayz");
+
+let firstNum= [];
+let secNum= [];
+let result =[];
 
 function add(a,b){
 return a+b;
@@ -10,32 +15,81 @@ function subtract(a,b){
     return a-b;
 };
 function multiply(a,b){
+
     return a*b;
 }
+
 function divide(a,b){
     return a/b;
 }
 
-function operator(a,b,c){
+function operator(event){
 if(c === 'plus')
-
 return add(a,b);
 }
-function populate (){
 
+function populate (event){
+    if(event.currentTarget.textContent === 'Clear')
+    {
+        display.innerHTML = '';
+        result=[];
+    }
+    else if(event.currentTarget.textContent === '=')
+    {
+        result[2] =firstNum.join('');
+        firstNum = [];
+        let a = +result[0];
+        let b = +result[2];
+        
+        if(result[1]=== '+'){
+            result[20] = a + b;
+        }
+        else if (result[1] === '-' )
+        {
+            result[20] = a-b;
+        }
+        else if( result[1]==='x'){
+            result[20] = a*b;
+        }
+        else if(result[1]===':')
+        {
+            result[20] = a/b;
+        }
+        
+        display.innerHTML = result[20];    
+    }
+    else if (result[2]){
+        let op=event.currentTarget.textContent;
+        result[4] =firstNum.join('');
+        result[3] =op;
+        display.innerHTML = op;
+        
+    } 
+    else if(result[3]){
+
+    }
+    else {
+    let op=event.currentTarget.textContent;
+    result[0] =firstNum.join('');
+    result[1] =op;
+    display.innerHTML = op;
+    firstNum=[];
+    }
 }
 
+const displays= (event) => {
 
-function displays (event){
-let num = event.currentTarget.className;
-let numy = num.textContent;
-display.innerHTML = numy;
-
+let num = event.currentTarget.textContent;
+firstNum.push(num);
+let first = firstNum.join('');
+display.innerHTML = first;
 
 }
-
-
 
 for (var i = 0 ; i < btns.length; i++) {
     btns[i].addEventListener('click' , displays) ; 
+ }
+
+ for (var j = 0 ; j < o.length; j++) {
+    o[j].addEventListener('click' , populate) ; 
  }
